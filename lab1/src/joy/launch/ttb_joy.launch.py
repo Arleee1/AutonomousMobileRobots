718234da2ffd5a449a7d9dff0945d750a8ecbe2e
Ben Kessler <BenPKessler@gmail.com>
2024-09-24 07:21:01 -0400

    remap launch

--- lab1/src/joy/launch/ttb_joy.launch.py
+++ lab1/src/joy/launch/ttb_joy.launch.py
@@ -1,13 +1,21 @@
 from launch import LaunchDescription
 from launch_ros.actions import Node
+from launch.substitutions import LaunchConfiguration
+from launch.actions import DeclareLaunchArgument
 
 def generate_launch_description():
     return LaunchDescription([
+        DeclareLaunchArgument(
+            'cmd_vel',
+            default_value='/TTB10/cmd_vel',
+            description='cmd_vel topic remap'
+        ),
         Node(
             package='joy',
             namespace='/TTB10/',
             executable='ttb_joy',
             name='ttb_joy',
             output='screen',
+            remappings=[('cmd_vel', LaunchConfiguration('cmd_vel'))],  # Remap cmd_vel
         )
     ])
