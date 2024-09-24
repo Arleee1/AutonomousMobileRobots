f7758cf85da874bd9b8e4cfa80ea46fc5038a7b6
Ben Kessler <BenPKessler@gmail.com>
2024-09-24 05:11:24 -0400

    launch with auto

--- lab1/src/state_machine/launch/state_machine.launch.py
+++ lab1/src/state_machine/launch/state_machine.launch.py
@@ -5,16 +5,18 @@ from launch.launch_description_sources import PythonLaunchDescriptionSource
 from ament_index_python.packages import get_package_share_directory
 
 def generate_launch_description():
-    # another_launch_file = IncludeLaunchDescription(
-    #     PythonLaunchDescriptionSource([get_package_share_directory('another_package'), '/launch/another_launch.py']),
-    #     launch_arguments={'some_arg': 'some_value'}.items(),
-    # )
+    wander_launch_file = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource([get_package_share_directory('wander'), '/launch/wander.launch.py']),
+        launch_arguments={'cmd_vel': '/TTB10/wander/cmd_vel'}.items(),  # remap cmd_vel topic
+    )
 
     return LaunchDescription([
-        # another_launch_file,
+        wander_launch_file,
         Node(
-            package='my_package',
-            executable='my_node',
-            name='my_node_name'
+            package='state_machine',
+            namespace='/TTB10/',
+            executable='state_machine_Node',
+            name='state_machine_Node',
+            output='screen',
         )
     ])
